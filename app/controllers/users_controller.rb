692012a5ed1cf605531b95c61998238633a6b143
stephenkoo <stephen.koo.yr@gmail.com>
2016-07-19 12:08:04 +1000

    Fix 'missing user/destroy template' error when deleting by redirecting to user_path.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def destroy
+		user = User.find(params[:id])
+		user.destroy
+		flash[:message] = "User deleted"
+		redirect_to users_path
 	end
 end
